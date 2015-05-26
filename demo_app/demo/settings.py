c09f522b6affad0ef4d37ab65bce856326b321e3
Tony REN <ren.dongni@gmail.com>
2015-05-26 17:37:32 +0800

    Update settings.py

--- demo_app/demo/settings.py
+++ demo_app/demo/settings.py
@@ -21,10 +21,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'django-xadmin',                      # Or path to database file if using sqlite3.
-        'USER': 'datamaster',                      # Not used with sqlite3.
-        'PASSWORD': 'datamaster',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_ROOT, 'data.db'),                       # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 # Hosts/domain names that are valid for this site; required if DEBUG is False
